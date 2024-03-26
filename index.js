88745ca8f111a175035db493253d533195801157
AidenG12 <Ajg0903@outlook.com>
2024-03-26 18:15:43 -0400

    learned console and let keywords

---
+++ index.js
@@ -0,0 +1,10 @@
+let number = Math.pow(7,2)
+let number2 =3
+console.log(number+number2)
+console.log(number-number2)
+console.log(number*number2)
+console.log(number/number2)
+console.log(number%number2)
+
+let planents = [ "mars","apple","pie"]
+console.log (planents[0])
---
+++ index1.js
@@ -0,0 +1,4 @@
+let fruits = ["apple","peach","pear","mango","kiwi"]
+console.log(Math.floor(Math.random() * fruits.length) + 1)
+let number = Math.floor(Math.random() * fruits.length) + 1
+console.log(fruits[number])
